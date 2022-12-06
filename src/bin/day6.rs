44c611ed0174227588255cf7e968f4996563b331
Damian Luna <damian.daniel.luna@gmail.com>
2022-12-06 05:24:19 -0300

    Use option

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -2,11 +2,11 @@ use std::{collections::VecDeque, fs};
 
 fn main() {
     let input = fs::read_to_string("inputs/day6.txt").expect("file should exist");
-    println!("Part 1: {}", get_start_of_message(&input, 4));
-    println!("Part 2: {}", get_start_of_message(&input, 14));
+    println!("Part 1: {}", get_start_of_message(&input, 4).unwrap());
+    println!("Part 2: {}", get_start_of_message(&input, 14).unwrap());
 }
 
-fn get_start_of_message(signal: &String, unique_chars: u8) -> usize {
+fn get_start_of_message(signal: &String, unique_chars: u8) -> Option<usize> {
     let mut packet_start = VecDeque::new();
     for (i, character) in signal.chars().enumerate() {
         if packet_start.contains(&character) {
@@ -14,8 +14,8 @@ fn get_start_of_message(signal: &String, unique_chars: u8) -> usize {
         }
         packet_start.push_back(character);
         if packet_start.len() == unique_chars as usize {
-            return i + 1;
+            return Some(i + 1);
         }
     }
-    panic!("no start of message");
+    None
 }
